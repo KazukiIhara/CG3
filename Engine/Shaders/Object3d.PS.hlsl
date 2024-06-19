7faefbcdc278cdb826d8ff210d4ddef61fc3b10f
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-06-19 16:40:02 +0900

    00_03無事完了

--- Engine/Shaders/Object3d.PS.hlsl
+++ Engine/Shaders/Object3d.PS.hlsl
@@ -12,12 +12,6 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     output.color = gMaterial.color;
     
-    // output.colorのa値が0の時にPixelを廃却
-    if (output.color.a == 0.0)
-    {
-        discard;
-    }
-    
     float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
 
@@ -25,7 +19,7 @@ PixelShaderOutput main(VertexShaderOutput input)
     if (textureColor.a <= 0.5)
     {
         discard;
-    }  
+    }
     
     if (gMaterial.enbleLighting != 0)
     {
@@ -38,5 +32,12 @@ PixelShaderOutput main(VertexShaderOutput input)
     {
         output.color = gMaterial.color * textureColor;
     }
+    
+    // output.colorのa値が0の時にPixelを廃却
+    if (output.color.a == 0.0)
+    {
+        discard;
+    }
+    
     return output;
 }
