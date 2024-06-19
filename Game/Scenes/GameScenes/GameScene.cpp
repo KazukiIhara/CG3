c844a29a7847ae34d33687229781448fe396d135
IharaKazuki <kazuto3996@gmail.com>
2024-06-20 02:46:31 +0900

    カメラの向きを調整 01_00無事完了

--- Game/Scenes/GameScenes/GameScene.cpp
+++ Game/Scenes/GameScenes/GameScene.cpp
@@ -35,7 +35,7 @@ void cGameScene::Initialize()
 	// テクスチャマネージャー初期化
 	cTextureManager::Initialize();
 	/*カメラ作成*/
-	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
+	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.1f,1.0f,0.0f},{0.0f,4.0f,10.0f} };
 	mainCamera_ = new cCameraController();
 	mainCamera_->Initialize(&cameraTransform_);
 
