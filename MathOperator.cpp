5dd90c2f371eaf109a565654c335a47c00123030
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-06-24 18:04:13 +0900

    オペランド作るなりよ

---
+++ MathOperator.cpp
@@ -0,0 +1,85 @@
+#include "MathOperator.h"
+
+Vector2 operator-(const Vector2& v) {
+	return Vector2();
+}
+
+Vector2 operator+(const Vector2& v) {
+	return Vector2();
+}
+
+Vector2 operator+(const Vector2& v1, const Vector2& v2) {
+	return Vector2();
+}
+
+Vector2 operator-(const Vector2& v1, const Vector2& v2) {
+	return Vector2();
+}
+
+Vector2 operator*(const Vector2& v1, const Vector2& v2) {
+	return Vector2();
+}
+
+Vector2 operator/(const Vector2& v1, const Vector2& v2) {
+	return Vector2();
+}
+
+Vector2 operator/(const Vector2& v, float s) {
+	return Vector2();
+}
+
+Vector2 operator*(float s, const Vector2& v) {
+	return Vector2();
+}
+
+Vector2 operator*(const Vector2& v, float s) {
+	return Vector2();
+}
+
+Vector3 operator-(const Vector3& v) {
+	return Vector3();
+}
+
+Vector3 operator+(const Vector3& v) {
+	return Vector3();
+}
+
+Vector3 operator+(const Vector3& v1, const Vector3& v2) {
+	return Vector3();
+}
+
+Vector3 operator-(const Vector3& v1, const Vector3& v2) {
+	return Vector3();
+}
+
+Vector3 operator*(const Vector3& v1, const Vector3& v2) {
+	return Vector3();
+}
+
+Vector3 operator/(const Vector3& v1, const Vector3& v2) {
+	return Vector3();
+}
+
+Vector3 operator*(float s, const Vector3& v) {
+	return Vector3();
+}
+
+Vector3 operator*(const Vector3& v, float s) {
+	return Vector3();
+}
+
+Vector3 operator/(const Vector3& v, float s) {
+	return Vector3();
+}
+
+Matrix4x4 operator+(const Matrix4x4& m1, const Matrix4x4& m2) {
+	return Matrix4x4();
+}
+
+Matrix4x4 operator-(const Matrix4x4& m1, const Matrix4x4& m2) {
+	return Matrix4x4();
+}
+
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) {
+	return Matrix4x4();
+}
